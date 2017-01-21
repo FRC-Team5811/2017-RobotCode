957684ebee3b327105176b98b396be08f07c6c07
wboylin <wdboylin@gmail.com>
2017-01-21 12:28:51 -0500

    changes since thursday

--- 2017-V_1.0.0/src/org/usfirst/frc/team5811/robot/Robot.java
+++ 2017-V_1.0.0/src/org/usfirst/frc/team5811/robot/Robot.java
@@ -8,6 +8,7 @@ import org.usfirst.frc.team5811.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class Robot extends IterativeRobot {
@@ -28,9 +29,8 @@ public class Robot extends IterativeRobot {
     JoystickButton logitechB;
     JoystickButton logitechLeftBumper;
     JoystickButton logitechRightBumper;
-
-    int direction; 
-    boolean stupidIntake;
+    JoystickButton logitechLeftStickPress;
+    JoystickButton logitechStart;
     
     //Motors
     Victor frontLeftDriveMotor;
@@ -38,10 +38,18 @@ public class Robot extends IterativeRobot {
     Victor backLeftDriveMotor;
     Victor backRightDriveMotor;
     Victor intake;
-    Victor shooter;
+    Spark shooterRight;;
+    Spark shooterLeft;
     Victor climber;
     Victor agitator;
     Victor elevator;
+    
+    //Encoder definitions and variables
+    Encoder shooterEnc;
+    int rotationCount;
+    double rotationRate;
+    boolean encDirection;
+    boolean encIfStopped;
     
     double intakePower; 
     
@@ -71,16 +79,12 @@ public class Robot extends IterativeRobot {
     boolean climberAscendStateChange;
     boolean climberDescendStateChange;
     boolean gearTrayStateChange;
-    
-    double leftTrim;
-    double rightTrim;
+    boolean leftStickPressStateChange;
     
     //Global Speed Values
     double leftSpeed;
     double rightSpeed;
     
-    double throttleGain;
-    double turningGain;
     
     double current;
     double n;
@@ -106,13 +110,29 @@ public class Robot extends IterativeRobot {
     double dpad;
     double logitechDPad;
     
+    
+  
     private void singleStickArcade(){
-        frontLeftDriveMotor.set(joyStickLeft.getY()+joyStickLeft.getX());
-        frontRightDriveMotor.set(joyStickLeft.getY()-joyStickLeft.getX());
-        backLeftDriveMotor.set(joyStickLeft.getY()+joyStickLeft.getX());
-        backRightDriveMotor.set(joyStickLeft.getY()-joyStickLeft.getY());
-        
+        frontLeftDriveMotor.set(-joyStickLeft.getY()+joyStickLeft.getX());
+        frontRightDriveMotor.set(-joyStickLeft.getY()-joyStickLeft.getX());
+        backLeftDriveMotor.set(-joyStickLeft.getY()+joyStickLeft.getX());
+        backRightDriveMotor.set(-joyStickLeft.getY()-joyStickLeft.getX());
     }
+    
+    private void driveMotors(double speedLeftDM, double speedRightDM) {
+    	//System.out.println("Command: " + speedLeftDM);
+    	frontLeftDriveMotor.set(speedLeftDM);
+    	frontRightDriveMotor.set(speedRightDM);
+    	backLeftDriveMotor.set(speedLeftDM);
+    	backRightDriveMotor.set(speedRightDM);
+    }
+    
+    
+    private void arcadeDrive(double throttle, double turn){
+    	driveMotors((throttle + turn), (throttle - turn));
+    }
+    
+    
     public void robotInit() {
  
 		oi = new OI();
@@ -124,32 +144,47 @@ public class Robot extends IterativeRobot {
         System.out.println(SmartDashboard.getBoolean("DB/Button 0", false));
   
         //Motor port instantiating
-       frontLeftDriveMotor = new Victor(1);
+       frontLeftDriveMotor = new Victor(0);
        frontRightDriveMotor = new Victor(8);
        backLeftDriveMotor = new Victor(0); 
        backRightDriveMotor = new Victor(9);
        
        //Accessory motors
-       intake = new Victor(4);
-       shooter = new Victor(5);
+       intake = new Victor(1);
+       shooterRight = new Spark(5);
+       shooterLeft = new Spark(4);
        climber = new Victor(3);
        elevator = new Victor(7);
        agitator = new Victor(6);
        
+       //Encoder inits and instantiations
+       shooterEnc = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
+       shooterEnc.setMaxPeriod(.1);
+       shooterEnc.setMinRate(10);
+       shooterEnc.setDistancePerPulse(5);
+       shooterEnc.setReverseDirection(true);
+       shooterEnc.setSamplesToAverage(7);
+       
+       rotationCount = shooterEnc.get();
+       rotationRate = shooterEnc.getRate();
+       encIfStopped = shooterEnc.getStopped();
+       encDirection = shooterEnc.getDirection();//since it is a boolean its either 0 or 1 (obv)...not sure which value is what direction though
+      
        joyStickLeft = new Joystick(0);
        joyStickRight = new Joystick(1);
        logitech = new Joystick(2);
-      
+       
        logitechY= new JoystickButton(logitech, 4);
        logitechA= new JoystickButton(logitech, 1);
        logitechX= new JoystickButton(logitech, 3);
        logitechB= new JoystickButton(logitech, 2);
        logitechLeftBumper= new JoystickButton(logitech, 5);
        logitechRightBumper= new JoystickButton(logitech, 6);
-       stupidIntake = false;
+       logitechLeftStickPress = new JoystickButton(logitech, 9);
+       logitechStart = new JoystickButton(logitech, 7);
        
-       rightTrim = SmartDashboard.getNumber("DB/Slider 3", 1.0);
-       if(rightTrim == 0){ SmartDashboard.putNumber("DB/Slider 3", 1); rightTrim = 1;}
+       //rightTrim = SmartDashboard.getNumber("DB/Slider 3", 1.0);
+       //if(rightTrim == 0){ SmartDashboard.putNumber("DB/Slider 3", 1); rightTrim = 1;}
        
        gearTrayCylinder = new DoubleSolenoid(2,1);//port 0 failed, changed to 2
        ballBlockCylinder = new DoubleSolenoid(3,4);
@@ -163,11 +198,8 @@ public class Robot extends IterativeRobot {
        compressor = new Compressor(0);
        compressor.setClosedLoopControl(false);
        
-       throttleGain = 1;
-       turningGain = .8;
-     
        //limit switch init
-       limitSwitch =  new DigitalInput(1);
+       //imitSwitch =  new DigitalInput(1);
        
        current = power.getCurrent(15);
        System.out.println(current);
@@ -179,8 +211,7 @@ public class Robot extends IterativeRobot {
        climberAscendStateChange = true;
        climberDescendStateChange = true;
        gearTrayStateChange = true;
-       
-    
+       leftStickPressStateChange = true;
     }
    
     private void operatorControl(){
@@ -216,26 +247,75 @@ public class Robot extends IterativeRobot {
 
         Scheduler.getInstance().run();
         
-        singleStickArcade();
+        if (leftStickPressStateChange){
+        	arcadeDrive(logitech.getRawAxis(0)+(logitech.getRawAxis(3)-logitech.getRawAxis(2)),logitech.getRawAxis(5));
+        	logitech.setRumble(RumbleType.kLeftRumble, 1);
+        	logitech.setRumble(RumbleType.kRightRumble, 1);
+        	if(logitechLeftStickPress.get()){
+            	leftStickPressStateChange = false;
+        	}
+        }else{
+        	singleStickArcade();
+        	logitech.setRumble(RumbleType.kLeftRumble, 1);
+        	logitech.setRumble(RumbleType.kRightRumble, 0);
+        	if(logitechLeftStickPress.get()){
+        		leftStickPressStateChange = true;
+        	}
+        }
         
         //agitator
         agitator.set(.75);
-        //shooter
-        if(logitechRightBumper.get() && shooterStateChange == true) {
+        if(logitechStart.get()){
+        	if(rotationCount < 100){ //might have to be changed
+        		shooterRight.set(.75);
+        		shooterLeft.set(.75);
+        		ballBlockCylinder.set(DoubleSolenoid.Value.kForward);
+        		if(rotationCount >= 100){
+        			SmartDashboard.putNumber("SHOOTER READY", 101);
+        		}
+        }
+        //shooter and block pneumatic
+        if(logitechRightBumper.get()){
+        	if(shooterStateChange){
+        		shooterRight.set(.75);
+        		shooterLeft.set(.75);
+        		ballBlockCylinder.set(DoubleSolenoid.Value.kReverse);
+            	shooterStateChange = false;
+            	
+        	}else{
+        		shooterRight.set(0);
+        		shooterLeft.set(0);
+        		shooterStateChange = true;
+        		ballBlockCylinder.set(DoubleSolenoid.Value.kForward);
+        	}
+        }
+       /*
+        if(isSet && shooterStateChange == true) {
         	shooter.set(.75);
         	shooterStateChange = false;
         	//if(encoderValue=100%){
         	//ballBlockCylinder.set(DoubleSolenoid.Value.kReverse);
         	//}
-        }	
-      
-        if(logitechRightBumper.get() && shooterStateChange == false){
+        }
+        if(isSet && shooterStateChange == false){
     		shooter.set(0);
     		shooterStateChange = true;
     		ballBlockCylinder.set(DoubleSolenoid.Value.kForward);
-    	}
+    	}*/
+        
         //intake
-    	if(logitechLeftBumper.get() && intakeStateChange == true){
+        if(logitechLeftBumper.get()){
+        	if(intakeStateChange){
+        		intake.set(1);
+        		SmartDashboard.putNumber("INTAKE ON", 101);
+        		intakeStateChange = false;
+        	}else{
+        		intake.set(0);
+        		SmartDashboard.putNumber("INTAKE OFF", 101);
+        		intakeStateChange = true; 
+        	}
+        }
+    	/*if(logitechLeftBumper.get() && intakeStateChange == true){
     		intake.set(1);
     		SmartDashboard.putNumber("INTAKE ON", 101);
     		intakeStateChange = false;
@@ -245,18 +325,39 @@ public class Robot extends IterativeRobot {
     		intake.set(0);
     		SmartDashboard.putNumber("INTAKE OFF", 101);
     		intakeStateChange = true; 
-    	}
+    	}*/
+    	
     	//climber down
-    	if(logitechA.get() && climberDescendStateChange == true){
+        if(logitechA.get()){
+        	if(climberDescendStateChange){
+        		climber.set(-1);
+        		climberDescendStateChange = false;
+        	}else{
+        		climber.set(0);
+        		climberDescendStateChange = true;
+        	}
+        }
+    	/*if(logitechA.get() && climberDescendStateChange == true){
     		climber.set(-1);
     		climberDescendStateChange = false;
     	}
     	if(logitechA.get() && climberDescendStateChange == false){
     		climber.set(0);
     		climberDescendStateChange = true;
-    	}
+    	}*/
+        
     	//climber up
-    	if(logitechY.get() && climberAscendStateChange == true){
+        if(logitechY.get()){
+        	if(climberAscendStateChange){
+        		climber.set(1);
+        		agitator.set(0);
+        		climberAscendStateChange = false;
+        	}else{
+        		climber.set(0);
+        		climberAscendStateChange = true;
+        	}
+        }
+    	/*if(logitechY.get() && climberAscendStateChange == true){
     		climber.set(1);
     		agitator.set(0);
     		climberAscendStateChange = false;
@@ -264,14 +365,27 @@ public class Robot extends IterativeRobot {
     	if(logitechY.get() && climberAscendStateChange == false){
     		climber.set(0);
     		climberAscendStateChange = true;
-    	}
+    	}*/
+    	
     	/*
     	if(current > n){
     		climber.set(0);
     	}
     	*/
+    	
     	//elevator
-    	if(logitechX.get() && elevatorStateChange == true){
+        if(logitechX.get()){
+        	if(elevatorStateChange){
+        		elevator.set(1);
+        		SmartDashboard.putNumber("ELEVATOR ON", 101);
+        		elevatorStateChange = false;
+        	}else{
+        		elevator.set(0);
+        		SmartDashboard.putNumber("ELEVATOR OFF", 101);
+        		elevatorStateChange = true;
+        	}
+        }
+    	/*if(logitechX.get() && elevatorStateChange == true){
     		elevator.set(1);
     		SmartDashboard.putNumber("ELEVATOR ON", 101);
     		elevatorStateChange = false;
@@ -280,15 +394,23 @@ public class Robot extends IterativeRobot {
     		elevator.set(0);
     		SmartDashboard.putNumber("ELEVATOR OFF", 101);
     		elevatorStateChange = true;
-    	}
+    	}*/
+    	
+    	//gear tray
+        if(logitechB.get()){
+        	if(gearTrayStateChange){
+        		gearTrayCylinder.set(DoubleSolenoid.Value.kForward);
+       		 SmartDashboard.putNumber("GEAR TRAY OUT", 101);
+       		 gearTrayStateChange = false;
+        	}else{
+        		gearTrayCylinder.set(DoubleSolenoid.Value.kReverse);
+       		 SmartDashboard.putNumber("GEAR TRAY IN", 101);
+       		 gearTrayStateChange = true;
+        	}
+        }
+        }
     	/*
-    	if(current > n){
-    		elevator.set(0);
-    		elevatorStateChange = true;
-    	}
-    	*/
-    	//geartray
-    	if(logitechB.get() && gearTrayStateChange == true){
+    	 if(logitechB.get() && gearTrayStateChange == true){
     		 gearTrayCylinder.set(DoubleSolenoid.Value.kForward);
     		 SmartDashboard.putNumber("GEAR TRAY OUT", 101);
     		 gearTrayStateChange = false;
@@ -298,6 +420,7 @@ public class Robot extends IterativeRobot {
     		 SmartDashboard.putNumber("GEAR TRAY IN", 101);
     		 gearTrayStateChange = true;
     	}
+    	*/
     }
     public void testPeriodic() {
         LiveWindow.run();  
